96db2a886e176e254d8ef48ce553a80cd7325ac2
Kamalani <kamalani@dev-island.org>
2022-06-30 14:07:35 -1000

    finished advice generator

--- app.js
+++ app.js
@@ -1,74 +1,18 @@
-// ATTEMPT 1
-const BASE_URL = "https://api.adviceslip.com";
+async function getAdvice() {
+  const response = await fetch("https://api.adviceslip.com/advice");
 
-const getAdvice = () =>
-  fetch(`${BASE_URL}/advice`)
-    .then((result) => {
-      if (!result.ok) return new Error("Oh no, it broken");
-      return result.json();
-    })
-    .catch((err) => {
-      return new Error(err);
-    });
+  const data = await response.json();
+  console.log(data.slip);
 
-getAdvice().then((advice) => {
-  console.log({ advice });
-});
+  displayData(data.slip);
 
-const adviceTemplate = document.querySelector("#advice-template");
-const renderQuote = (quote) => {
-  const adviceElement = adviceTemplate.content.cloneNode(true);
-  adviceElement.querySelector(".advice").innerText = quote.advice;
-  advice.innerText = quote.advice;
-  return adviceElement;
-};
+  displayId(data.slip);
 
-const app = document.querySelector("#app");
-const renderCards = async () => {
-  const advice = await advice();
+  function displayData(data) {
+    document.getElementById("advice").innerHTML = `"${data.advice}"`;
+  }
 
-  console.log({ advice });
-
-  advice.results.forEach((quote) => {
-    app.appendChild(renderQuote(quote));
-  });
-};
-
-renderCards();
-
-// ATTEMPT 2
-
-// const dice = document.querySelector(".dice_wrapper");
-// const id = document.querySelector(".advice_num");
-// const advice = document.querySelector(".advice");
-
-// function loadQuote() {
-//   fetch("https://api.adviceslip.com/advice")
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((response) => {
-//       id.innerHTML = `Advice #${response.slip.id}`;
-//       advice.innerHTML = `&#8220${response.slip.advice}&#8221`;
-//     });
-// }
-
-// dice.addEventListener("click", loadQuote);
-
-// ATTEMPT 3
-
-// const button = document.querySelector("button");
-// const id = document.querySelector("#advice_num");
-// const advice = document.querySelector(".advice");
-
-// const url = "https://api.adviceslip.com/advice";
-
-// button.addEventListener("click", async () => {
-//   const response = await fetch(url);
-//   const json = await response.json();
-//   const { advice, id } = json.slip;
-//   id.textContent = id;
-//   advice.textContent = advice;
-// });
-
-// button.addEventListener("click");
+  function displayId(data) {
+    document.getElementById("id").innerHTML = `Advice #${data.id}`;
+  }
+}
